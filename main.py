34cef5a576b3ca9f8104880125537eed8c1e956e
davidedema <demartini.davide01@gmail.com>
2026-02-11 11:19:55 +0100

    init repo, added main files and requirements

---
+++ main.py
@@ -0,0 +1,13 @@
+# Example
+from pymycobot import MyCobot280
+import time
+import numpy as np
+from planner import TrajectoryPlanner
+# from controller import EndEffectorController
+
+
+def main():
+    planner = TrajectoryPlanner()
+
+if __name__ == "__main__":
+    main()
